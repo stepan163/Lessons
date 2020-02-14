bc25eed18a71812a78c26dfa449a4e6e24f14c45
stepan <stepan.paruna@yandex.ru>
2020-02-14 22:14:31 +0300

    Come back after 2 months. C++ here we come!

---
+++ 1/Examples/cout.cpp
@@ -0,0 +1,10 @@
+namespace std {
+    extern ostream cout;
+}
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello!\n";
+}
---
+++ 1/Examples/printsmth.cpp
@@ -0,0 +1,9 @@
+extern ostream cout;
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!\n";
+    return 228;
+}
